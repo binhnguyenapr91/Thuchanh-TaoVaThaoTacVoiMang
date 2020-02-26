66ad4fa7535bf89c6768a84d4f8152418760f3d5
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-02-26 09:00:30 +0700

    first commit

---
+++ js/this.js
@@ -0,0 +1,20 @@
+let inputArray = [];
+
+//function to add new array item to the end of array
+function addElement() {
+    let inputEle = document.getElementById("arrElement").value;
+    inputArray.push(inputEle);
+}
+
+
+//function to create a new p element,which display value of array in it's html
+function displayElement() {
+    for (let index=0;index<inputArray.length;index++){
+       let newElement = document.createElement("p");
+       newElement.className= "addedElement";
+       let parentElement = document.getElementById("result");
+       parentElement.appendChild(newElement);
+       document.getElementsByClassName("addedElement").item(index).innerHTML = inputArray[index];
+    }
+
+}
